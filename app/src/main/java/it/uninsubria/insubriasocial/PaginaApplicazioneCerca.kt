be81c3bc50254d1c77f86beeccdb33552a8318ae
RadSamu <samueleradice01@gmail.com>
2024-04-29 14:42:11 +0200

    Update generali

--- app/src/main/java/it/uninsubria/insubriasocial/PaginaApplicazioneCerca.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaApplicazioneCerca.kt
@@ -1,6 +1,8 @@
 package it.uninsubria.insubriasocial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,26 @@ class PaginaApplicazioneCerca : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.btnHome2).setOnClickListener {
+            val pgHome = Intent(this, PaginaApplicazioneHome::class.java)
+            startActivity(pgHome)
+        }
+
+        findViewById<Button>(R.id.btnMap2).setOnClickListener {
+            val pgMappa = Intent(this, PaginaApplicazioneMappa::class.java)
+            startActivity(pgMappa)
+        }
+
+        findViewById<Button>(R.id.btnDashboard2).setOnClickListener {
+            val pgBacheca = Intent(this, PaginaApplicazioneBacheca::class.java)
+            startActivity(pgBacheca)
+        }
+
+        findViewById<Button>(R.id.btnProfile2).setOnClickListener {
+            val pgProfilo = Intent(this, PaginaApplicazioneProfilo::class.java)
+            startActivity(pgProfilo)
+        }
+
     }
 }
--- app/src/main/java/it/uninsubria/insubriasocial/PaginaApplicazioneProfilo.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaApplicazioneProfilo.kt
@@ -1,6 +1,8 @@
 package it.uninsubria.insubriasocial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,26 @@ class PaginaApplicazioneProfilo : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Button>(R.id.btnHome5).setOnClickListener {
+            val pgHome = Intent(this, PaginaApplicazioneHome::class.java)
+            startActivity(pgHome)
+        }
+
+        findViewById<Button>(R.id.btnSearch5).setOnClickListener {
+            val pgCerca = Intent(this, PaginaApplicazioneCerca::class.java)
+            startActivity(pgCerca)
+        }
+
+        findViewById<Button>(R.id.btnMap5).setOnClickListener {
+            val pgMappa = Intent(this, PaginaApplicazioneMappa::class.java)
+            startActivity(pgMappa)
+        }
+
+        findViewById<Button>(R.id.btnDashboard5).setOnClickListener {
+            val pgBacheca = Intent(this, PaginaApplicazioneBacheca::class.java)
+            startActivity(pgBacheca)
         }
     }
 }
