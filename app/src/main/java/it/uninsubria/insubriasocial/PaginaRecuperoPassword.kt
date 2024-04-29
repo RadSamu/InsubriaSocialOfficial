a51d3edc005e413675361d829dd5e9d76524a9db
Iacopo <ialcasalini@studenti.uninsubria.it>
2024-04-29 15:36:48 +0200

    Aggiunta funzione di recupero password

--- app/src/main/java/it/uninsubria/insubriasocial/PaginaRecuperoPassword.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaRecuperoPassword.kt
@@ -1,14 +1,26 @@
 package it.uninsubria.insubriasocial
 
 import android.content.Intent
+import android.icu.text.IDNA.Info
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.ktx.Firebase
 
 class PaginaRecuperoPassword : AppCompatActivity() {
+    val db = FirebaseFirestore.getInstance()
+    val Utenti = db.collection("InsubriaSocial_Utenti")
+    lateinit var auth: FirebaseAuth
+    private lateinit var eMail: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,9 +31,16 @@ class PaginaRecuperoPassword : AppCompatActivity() {
             insets
         }
 
+
         findViewById<Button>(R.id.btnAvanti).setOnClickListener {
-            val fw = Intent(this, PaginaRecuperoPasswordPt2::class.java)
-            startActivity(fw)
+            eMail = findViewById<EditText>(R.id.editTextTextEmailAddress2)
+            val userEMail = eMail.text.toString().trim()
+            val intent: Intent = Intent(this, PaginaRecuperoPasswordPt2::class.java).apply {
+                putExtra("eMail", userEMail)
+            }
+
+            startActivity(intent)
+            }
+
         }
     }
-}
--- app/src/main/java/it/uninsubria/insubriasocial/PaginaRecuperoPasswordPt2.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaRecuperoPasswordPt2.kt
@@ -3,12 +3,26 @@ package it.uninsubria.insubriasocial
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.ktx.Firebase
 
 class PaginaRecuperoPasswordPt2 : AppCompatActivity() {
+    val db = FirebaseFirestore.getInstance()
+    val Utenti = db.collection("InsubriaSocial_Utenti")
+    lateinit var auth: FirebaseAuth
+    private lateinit var nuovaPassword: EditText
+    private lateinit var confermaNuovaPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,9 +33,50 @@ class PaginaRecuperoPasswordPt2 : AppCompatActivity() {
             insets
         }
 
+
+        nuovaPassword = findViewById<EditText>(R.id.editTextRecPsw)
+        confermaNuovaPassword = findViewById<EditText>(R.id.editTextConfRecPsw)
+
         findViewById<Button>(R.id.btnRecuperaPsw).setOnClickListener {
-            val recupera = Intent(this, PaginaRecuperoPasswordSuccesso::class.java)
-            startActivity(recupera)
+
+            val recupera = Intent(this, PaginaAccesso::class.java)
+            val address = intent.getStringExtra("eMail")
+            val psw = nuovaPassword.text.toString().trim()
+            val confPsw = confermaNuovaPassword.text.toString().trim()
+
+
+            // Query per cercare l'utente
+            val srcQuery: Query =
+                db.collection("InsubriaSocial_Utenti")
+                    .whereEqualTo("eMail", address)
+            srcQuery.get().addOnCompleteListener{task ->
+                if(task.isSuccessful){
+                    for(document in task.result) {
+                        val userMail = document.getString("eMail")
+                        if(userMail == address && (psw == confPsw) && (psw != document.getString("password"))){
+                            //db.collection("InsubriaSocial_Utenti").document().update("password", psw)
+                            val update: MutableMap<String, Any> = HashMap()
+                            update["password"] = psw
+                            Utenti.document(document.id).set(update, SetOptions.merge())
+                            startActivity(recupera)
+                            Toast.makeText(
+                                this,
+                                "Password reimpostata con successo!",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }else{
+                            Toast.makeText(
+                                this,
+                                "Errore, controlla i campi e riprova!",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+                }
+            }
+
+
+
         }
     }
 }
