130f69d79f3a497e72431141f746a10cc4a1a59d
Iacopo <ialcasalini@studenti.uninsubria.it>
2024-05-02 15:52:11 +0200

    Salvataggio generale.

--- app/src/main/java/it/uninsubria/insubriasocial/PaginaAggiungiBacheca.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaAggiungiBacheca.kt
@@ -1,14 +1,23 @@
 package it.uninsubria.insubriasocial
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.time.LocalDate
 
 class PaginaAggiungiBacheca : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +27,18 @@ class PaginaAggiungiBacheca : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
             findViewById<Button>(R.id.btnIndietro).setOnClickListener{
-                val pgApplicazioneBacheca = Intent(this, PaginaApplicazioneBacheca::class.java)
-                startActivity(pgApplicazioneBacheca)
+                val tornaIndietro = Intent(this, PaginaApplicazioneBacheca::class.java)
+                startActivity(tornaIndietro)
             }
 
+        findViewById<Button>(R.id.btnInvia).setOnClickListener{
+            val pubblicaAnnuncio = Intent(this, PaginaApplicazioneHome::class.java)
+            startActivity(pubblicaAnnuncio)
+        }
+
     }
+
 }
+
