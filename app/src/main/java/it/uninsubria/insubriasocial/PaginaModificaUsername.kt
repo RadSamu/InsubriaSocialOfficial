477ac4b11f25675ecba5ac6a1138567382e3c31a
RadSamu <samueleradice01@gmail.com>
2024-05-09 15:39:15 +0200

    Update generali

--- app/src/main/java/it/uninsubria/insubriasocial/PaginaModificaUsername.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaModificaUsername.kt
@@ -26,7 +26,7 @@ class PaginaModificaUsername : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pagina_recupero_password)
+        setContentView(R.layout.activity_pagina_modifica_username)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/it/uninsubria/insubriasocial/PaginaModificaUsernamePt2.kt
+++ app/src/main/java/it/uninsubria/insubriasocial/PaginaModificaUsernamePt2.kt
@@ -28,7 +28,7 @@ class PaginaModificaUsernamePt2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pagina_recupero_password_pt2)
+        setContentView(R.layout.activity_pagina_modifica_username_pt2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
